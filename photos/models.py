0da7d9a05160cd70afa0d5835b2d98dc388e9294
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-22 09:29:24 +0300

    list models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,12 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    pass
+
+class Category(models.Model):
+    pass
+
+class Location(models.Model):
+    pass
