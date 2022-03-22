c2c1364618bec01e5d15fa79ef83283f36a6a593
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-22 21:48:24 +0300

    migrate

--- photos/models.py
+++ photos/models.py
@@ -3,10 +3,22 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    pass
+    image = models.ImageField(upload_to = 'articles/')
+    name = models.CharField(max_length=48)
+    description = models.CharField(max_length= 100)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Category(models.Model):
-    pass
+    categoryx = models.CharField(max_length=18)
+
+    def __str__(self):
+        return self.categoryx
 
 class Location(models.Model):
-    pass
+    locationx = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.locationx
