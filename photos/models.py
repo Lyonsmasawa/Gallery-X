2834238783b2c77d8ea9d4df57e0a39f88fc5b9b
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-23 07:11:49 +0300

    add relationships

--- photos/models.py
+++ photos/models.py
@@ -2,15 +2,6 @@ from unicodedata import category
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    image = models.ImageField(upload_to = 'articles/')
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length= 100)
-    post_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self) -> str:
-        return self.name
-
 class Category(models.Model):
     categoryx = models.CharField(max_length=20)
 
@@ -18,7 +9,18 @@ class Category(models.Model):
         return self.categoryx
 
 class Location(models.Model):
-    locationx = models.CharField(max_length=25)
+    locationx = models.CharField(max_length=25)    
 
     def __str__(self):
         return self.locationx
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'articles/')
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length= 100)
+    post_date = models.DateTimeField(auto_now_add=True)
+    image_category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    image_location = models.ForeignKey(Location,on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.name
