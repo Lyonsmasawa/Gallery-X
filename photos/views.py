cd1a4a738cc6b86cb75aa4b080f4bb147ef0217e
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-23 17:18:28 +0300

    needed views

--- photos/views.py
+++ photos/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def gallery(request):
-    return render
+    return render(request, 'photos/gallery.html')
+
+def viewImage(request):
+    return render(request, 'photos/image.html')
+
+def addImage(request):
+    return render(request, 'photos/add_image.html')
