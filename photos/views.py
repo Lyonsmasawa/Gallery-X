232a20a1fe3ef0c1c8320e5ca9d0ae7e4ef39ea5
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-23 17:14:45 +0300

    add templates

--- photos/views.py
+++ photos/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def trial(request):
-    return HttpResponse('test one')
+def gallery(request):
+    return render
