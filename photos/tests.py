39274c024fe20afb8c8cc73427dc5d912249afc5
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-23 08:41:03 +0300

    create tests for Location class

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Location, Category, Image
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    
+    def setUp(self):
+        """
+        create a location
+        """
+        Location.objects.create(locationx = 'Rongai')
+        self.location = Location.objects.get(locationx = "Rongai")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
