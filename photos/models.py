4a6bc8644cfed7df65c8529bf50d39ed774e2b6a
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-24 09:09:47 +0300

    add get all images function

--- photos/models.py
+++ photos/models.py
@@ -49,5 +49,10 @@ class Image(models.Model):
     def delete_image(self):
         self.delete()
 
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.all()
+        return images
+
     def __str__(self) -> str:
         return self.name
