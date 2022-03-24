b06ff5843d3c22624bbc976860d5f7de6d364c0c
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-24 08:30:00 +0300

    image page

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Image, Category, Location
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
--- photos/urls.py
+++ photos/urls.py
@@ -1,14 +1,13 @@
-from unicodedata import name
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import re_path
+from django.urls import re_path, path
 from . import views
 
 urlpatterns = [
-    re_path('', views.gallery, name = 'gallery'),
-    re_path('image/<str:pk>/', views.viewImage, name='image'),
-    re_path('add/', views.addImage, name = 'add')
+    path('', views.gallery, name = 'gallery'),
+    path('image/<int:pk>/', views.viewImage, name= 'image'),
+    path('add/', views.addImage, name = 'add')
 ]
 
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
