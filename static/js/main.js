62b9ff82a137e7dd2b0e44b47d19927de5c5e8de
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-28 00:48:54 +0300

    add copy link

--- static/js/main.js
+++ static/js/main.js
@@ -6,6 +6,6 @@ $('#clickImage').on('show.bs.modal', function (event) {
     modal.find('.modal.title').text(img_src)
 })
 
-function share(){
-    
+function copyToClipboard(text) {
+    window.prompt("Copy to clipboard:  Ctrl+C,  Enter", text);
 }
