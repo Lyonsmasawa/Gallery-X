a4b9fed720dc430ccfa14aef9660cf939412da9d
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-28 08:24:50 +0300

    style modal

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,13 @@ function copyToClipboard(text) {
     window.prompt("Ctrl+C to copy link,  Enter", texts);
     alert("hope that works :)")
 }
+
+$('.image-link').click(function () {
+    $('#image-name').html($(this).data('name'));
+    $('#image-description').html($(this).data('description'));
+    $('#image-description').html($(this).data('description'));
+    $('#image-category').html($(this).data('category'));
+    $('#image-location').html($(this).data('location'));
+    $('#image-date').html($(this).data('date'));
+    $('#copy').html($(this).data('img'));
+});
