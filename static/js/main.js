32e765cc8a56fcf5a983eff8b1b0d943627e3ac3
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-28 07:55:24 +0300

    add modal

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,3 @@
-$(document).ready(function () {
-    $('#clickImage').on('show.bs.modal', function (event) {
-        let image = $(event.relatedTarget)
-        let img_src = image.data("whatever")
-
-        var modal = $(this)
-        modal.find('.modal.title').text(img_src)
-    })
-});
-
 function copyToClipboard(text) {
     texts = window.location.origin + text
     window.prompt("Ctrl+C to copy link,  Enter", texts);
