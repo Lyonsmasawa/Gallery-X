ab866abe0c6dbd1e1d8eceb49aa09ae6deab9f21
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-28 09:25:24 +0300

    setup deployment settings

--- galleryx/settings.py
+++ galleryx/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG')
 
 # development
 if config('MODE')=="dev":
